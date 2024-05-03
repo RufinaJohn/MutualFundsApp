ada8141889c1b8c463aa9a301d7711d86fe94f88
Rufina <108195662+RufinaJohn@users.noreply.github.com>
2024-05-03 16:15:28 +0530

    Added tabs and updated report upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-import pandas
 import streamlit as st
 import pandas as pd
 import schemetypedata as stdata
@@ -6,37 +5,51 @@ import dataextraction as dataex
 
 st.title("RAGathon Challenge: Mutual Fund Performance Insights Application")
 df = pd.read_csv('mutualfunds.csv')
-monthselected = st.multiselect('Select the month', df['month and year'].unique())
-typeselected = st.multiselect('Select the scheme type', stdata.types)
-subtypeselected = st.multiselect('Select the scheme subtype', stdata.subtypes)
-namesselected = st.multiselect('Select the scheme name', stdata.names)
-result = st.button('Generate Report')
-if result:
-    res = df[
-        (df['month and year'].isin(monthselected)) &
-        ((df['Scheme Type'].isin(typeselected)) |
-         (df['Scheme Subtype'].isin(subtypeselected)) |
-         (df['Scheme Name'].isin(namesselected)))
-    ]
-    res.columns = dataex.column_headers
-    st.write(res)
 
+tab1, tab2, tab3 = st.tabs(['Generate report', 'Visualize data', 'Upload reports'])
+with tab1:
+    monthselected = st.multiselect('Select the month', df['month and year'].unique())
+    typeselected = st.multiselect('Select the scheme type', stdata.types)
+    subtypeselected = st.multiselect('Select the scheme subtype', stdata.subtypes)
+    namesselected = st.multiselect('Select the scheme name', stdata.names)
+    result = st.button('Generate Report')
+    if result:
+        res = df[
+            (df['month and year'].isin(monthselected)) &
+            ((df['Scheme Type'].isin(typeselected)) |
+            (df['Scheme Subtype'].isin(subtypeselected)) |
+            (df['Scheme Name'].isin(namesselected)))
+        ]
+        res.columns = dataex.column_headers
+        st.dataframe(res)
 
-st.write("Report PDF Upload")
-pdf = st.file_uploader("Upload a PDF file", type=["pdf"])
-submit = st.button("Update data")
-if submit:
-    if pdf is not None:
-        pdf_bytes = pdf.read()
-        pdf_path = "reports/{}.pdf".format(pdf.name)
-        with open(pdf_path, "wb") as f:
-            f.write(pdf_bytes)
-        newdf = pd.DataFrame(columns=dataex.column_headers)
-        tables = dataex.get_tables(pdf_path)
-        for tbname in tables:
-            table = tables[tbname]
-            newdf = dataex.add_data_to_dataframe(table, tbname, newdf)
-        df = df._append(newdf, ignore_index=True)
-        df.to_csv('mutualfunds.csv', index=False)
-        st.write('Data updated successfully')
+with tab2:
+    namesselected = st.multiselect('Select the scheme name', stdata.names)
+
+with tab3:
+    st.write("Report PDF Upload")
+    pdfs = st.file_uploader("Upload a PDF file", type=["pdf"], accept_multiple_files=True, help="Select one or more reports in the standard pdf format to update the existing data...")
+    submit = st.button("Update data")
+    if submit:
+        if len(pdfs) != 0:
+            with st.spinner(text='Extracting data...'):
+                bar = st.progress(0)
+                p = 0
+                for pdf in pdfs:
+                    pdf_bytes = pdf.read()
+                    pdf_path = "reports/{}.pdf".format(pdf.name)
+                    with open(pdf_path, "wb") as f:
+                        f.write(pdf_bytes)
+                    newdf = pd.DataFrame(columns=dataex.column_headers)
+                    tables = dataex.get_tables(pdf_path)
+                    for tbname in tables:
+                        table = tables[tbname]
+                        newdf = dataex.add_data_to_dataframe(table, tbname, newdf)
+                    df = df._append(newdf, ignore_index=True)
+                    df.drop_duplicates(inplace=True)
+                    bar.progress(p+int(100/len(pdfs)))
+                bar.progress(100)
+                df.to_csv('mutualfunds.csv', index=False)
+                st.success('Done')
+                st.rerun()
 
