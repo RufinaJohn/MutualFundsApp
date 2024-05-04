d2afa41b3ea1488c40291f8bd62c1355151f90e7
Rufina <108195662+RufinaJohn@users.noreply.github.com>
2024-05-05 00:32:58 +0530

    Version 3
    Pdf uploader does not upload files already uploaded, download old reports

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import streamlit as st
+import os
 import dataextraction as dataex
 import schemetypedata as stdata
 
@@ -12,7 +13,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 st.title("RAGathon Challenge: Mutual Fund Performance Insights Application")
 df = pd.read_csv('mutualfunds.csv')
 
-tab1, tab2, tab3 = st.tabs(['Generate report', 'Visualize data', 'Upload reports'])
+tab1, tab2, tab3, tab4 = st.tabs(['Generate report', 'Visualize data', 'Upload reports', 'Report PDFs'])
 with tab1:
     st.markdown('#### Generate report in csv format')
     option = st.radio('Get data based on ', ['Scheme name', 'Scheme type', 'Scheme subtype'], key='tb1')
@@ -86,7 +87,7 @@ with tab1:
             if col == 'slno':
                 continue
             elif col in numeric_columns:
-                new_row_data[col] = column_sums[col]  
+                new_row_data[col] = column_sums[col]
             else:
                 new_row_data[col] = ''
 
@@ -107,13 +108,13 @@ with tab2:
     st.markdown('#### Plot the the graph of data fields vs month and year')
     option = st.radio('Get data based on ', ['Scheme name', 'Scheme type', 'Scheme subtype'], key='tb2')
     if option == 'Scheme name':
-        title = nameselected = st.selectbox('Select the scheme name', stdata.names, key='sn2')
+        title = nameselected = st.selectbox('Select the scheme name', stdata.names, key='sn2', index=None)
         filtered_df = df[df['Scheme Name'] == nameselected]
     elif option == 'Scheme type':
-        title = typeselected = st.selectbox('Select the scheme type', stdata.types, key='st2')
+        title = typeselected = st.selectbox('Select the scheme type', stdata.types, key='st2', index=None)
         filtered_df = df[df['Scheme Type'] == typeselected]
     elif option == 'Scheme subtype':
-        title = subtypeselected = st.selectbox('Select the scheme subtype', subtypes, key='sst2')
+        title = subtypeselected = st.selectbox('Select the scheme subtype', subtypes, key='sst2', index=None)
         if(subtypeselected=='Income/Debt Oriented Schemes (Open ended)'):
             filtered_df = df[(df['Scheme Subtype'] == 'Income/Debt Oriented Schemes') & (df['Scheme Type'] == 'Open ended Schemes')]
         elif subtypeselected == 'Growth/Equity Oriented Schemes (Open ended)':
@@ -137,52 +138,69 @@ with tab2:
         elif subtypeselected == 'Other Schemes (Interval)':
             filtered_df = df[(df['Scheme Subtype'] == 'Other Schemes') & (df['Scheme Type'] == 'Interval Schemes')]
 
-    foption = st.selectbox('Select data field ', stdata.fields)
-    result = filtered_df[['{}'.format(foption), 'month and year']]
-    result['month and year'] = pd.to_datetime(result['month and year'], errors='coerce')
-    result = result.sort_values('month and year')
-    result = result.groupby('month and year')['{}'.format(foption)].sum().reset_index()
-    #st.dataframe(result)
+    foption = st.selectbox('Select data field ', stdata.fields, index=None)
+    if title is not None and foption is not None:
+        result = filtered_df[['{}'.format(foption), 'month and year']]
+        result['month and year'] = pd.to_datetime(result['month and year'], errors='coerce')
+        result = result.sort_values('month and year')
+        result = result.groupby('month and year')['{}'.format(foption)].sum().reset_index()
+        #st.dataframe(result)
 
-    # Plot the line graph
-    plt.style.use('dark_background')
-    plt.figure(figsize=(15,5))
-    plt.ticklabel_format(style='plain')
-    plt.plot(result['month and year'], result['{}'.format(foption)], color='#00CEFF', marker='o', linestyle='-')
-    date_format = mdates.DateFormatter('%m-%Y')
-    plt.gca().xaxis.set_major_formatter(date_format)
-    plt.title('{}'.format(title), color='#FFFFFF', fontweight='bold', fontsize=16)
-    plt.xlabel('Month and Year', color='#00CEFF')
-    plt.ylabel('{}'.format(foption), color='#00CEFF')
-    plt.xticks(rotation=45)
-    plt.grid(True)
-    plt.tight_layout()
-    st.pyplot()
+        # Plot the line graph
+        plt.style.use('dark_background')
+        plt.figure(figsize=(15,5))
+        plt.ticklabel_format(style='plain')
+        plt.plot(result['month and year'], result['{}'.format(foption)], color='#00CEFF', marker='o', linestyle='-')
+        date_format = mdates.DateFormatter('%m-%Y')
+        plt.gca().xaxis.set_major_formatter(date_format)
+        plt.title('{}'.format(title), color='#FFFFFF', fontweight='bold', fontsize=16)
+        plt.xlabel('Month and Year', color='#00CEFF')
+        plt.ylabel('{}'.format(foption), color='#00CEFF')
+        plt.xticks(rotation=45)
+        plt.grid(True)
+        plt.tight_layout()
+        st.pyplot()
 
 with tab3:
     st.markdown('#### Report PDF Upload')
     pdfs = st.file_uploader("Upload a PDF file", type=["pdf"], accept_multiple_files=True, help="Select one or more reports in the standard pdf format to update the existing data...")
     submit = st.button("Update data")
+    oldreports = [file for file in os.listdir('reports') if file.endswith('.pdf')]
     if submit:
         if len(pdfs) != 0:
             with st.spinner(text='Extracting data...'):
-                bar = st.progress(0)
-                p = 0
                 for pdf in pdfs:
-                    pdf_bytes = pdf.read()
-                    pdf_path = "reports/{}.pdf".format(pdf.name)
-                    with open(pdf_path, "wb") as f:
-                        f.write(pdf_bytes)
-                    newdf = pd.DataFrame(columns=dataex.column_headers)
-                    tables = dataex.get_tables(pdf_path)
-                    for tbname in tables:
-                        table = tables[tbname]
-                        newdf = dataex.add_data_to_dataframe(table, tbname, newdf)
-                    df = df._append(newdf, ignore_index=True)
-                    df.drop_duplicates(inplace=True)
-                    bar.progress(p+int(100/len(pdfs)))
-                bar.progress(100)
-                df.to_csv('mutualfunds.csv', index=False)
-                st.success('Done')
-                st.rerun()
-
+                    if pdf.name in oldreports:
+                        st.write('You have already uploaded the report - {}'.format(pdf.name))
+                        continue
+                    else:
+                        bar = st.progress(0)
+                        p = 0
+                        pdf_bytes = pdf.read()
+                        pdf_path = "reports/{}".format(pdf.name)
+                        with open(pdf_path, "wb") as f:
+                            f.write(pdf_bytes)
+                        newdf = pd.DataFrame(columns=dataex.column_headers)
+                        tables = dataex.get_tables(pdf_path)
+                        for tbname in tables:
+                            table = tables[tbname]
+                            newdf = dataex.add_data_to_dataframe(table, tbname, newdf)
+                        df = df._append(newdf, ignore_index=True)
+                        df.drop_duplicates(inplace=True)
+                        bar.progress(p+int(100/len(pdfs)))
+                    bar.progress(100)
+                    df.to_csv('mutualfunds.csv', index=False)
+                    st.success('Done')
+                    st.rerun()
+with tab4:
+    pdf_files = [file for file in os.listdir('reports') if file.endswith('.pdf')]
+    if pdf_files:
+        st.markdown("#### Download available Report PDFs")
+        for pdf_file in pdf_files:
+            download_path = os.path.join('reports', pdf_file)
+            st.download_button(
+                label="{}".format(pdf_file),
+                data=open(download_path, "rb").read(),
+                file_name=pdf_file,
+                mime="application/pdf"
+            )
